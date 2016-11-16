568a1295daeb010742065d5e0a297a142e9a2960
Phil Crump <phil@philcrump.co.uk>
2016-11-16 11:32:37 +0000

    Web rework. 4/3

--- build-openwrt.sh
+++ build-openwrt.sh
@@ -33,7 +33,7 @@ cp $BASE_DIR/ledeconfig-diff $LEDE_DIR/.config;
 
 timestamp=$(date +"%Y-%m-%d-%H%M")
 gitref=$(git describe --dirty --always)
-echo "$timestamp ($gitref)" > "$LEDE_DIR/files/etc/version"
+echo "{'p':'ar150','t':'$timestamp','g':'$gitref'}" > "$LEDE_DIR/files/etc/version"
 
 # Compile!
 cd $LEDE_DIR/;
